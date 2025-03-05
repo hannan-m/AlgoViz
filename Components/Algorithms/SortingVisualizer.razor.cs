0e31d6ebeb80fbabcf4577fd76a73cf7cac66748
Muhammad Hannan <muhammad.hannan@icon.con.mt>
2025-03-05 20:23:31 +0100

    remove old structure

--- Components/Algorithms/SortingVisualizer.razor.cs
+++
@@ -1,8 +0,0 @@
-using AlgorithmVisualizer.Core.Algorithms;
-using Microsoft.AspNetCore.Components;
-
-namespace AlgorithmVisualizer.Components.Algorithms;
-
-public partial class SortingVisualizer : ComponentBase
-{
-}
